7eab8f31c3e622009aef5b4b504d3374bd93b73f
Andreas Ziegler <github@andreas-ziegler.de>
2017-02-01 03:31:49 +0100

    vpnlimittimeclock: cleanup & indention fix

--- tecff-vpnlimittimeclock/files/lib/gluon/vpnlimittimeclock/vpnlimittimeclock.sh
+++ tecff-vpnlimittimeclock/files/lib/gluon/vpnlimittimeclock/vpnlimittimeclock.sh
@@ -8,7 +8,6 @@ vpnlimitoff="/tmp/vpnlimit.off"
 vpnlimiton="/tmp/vpnlimit.on"
 
 CurrentTime="$(date +%k%M)"
-CurrentDayOfWeek="$(date +%w)"
 
 dummy=$(uci get simple-tc.mesh_vpn.enabled)
 if [ $? -eq 0 ]; then
@@ -16,27 +15,27 @@ if [ $? -eq 0 ]; then
   if [ $? -eq 0 ]; then
     vpnlimiton=$(uci get simple-tc.mesh_vpn.clock_on)
     vpnlimitoff=$(uci get simple-tc.mesh_vpn.clock_off)
-    if ( [ ${#vpnlimiton} -eq 4 ] ) && ( [ ${#vpnlimitoff} -eq 4 ] ) ; then
-      if ( ( ( [ $vpnlimiton -le $vpnlimitoff ] ) && ( ( [ $CurrentTime -ge $vpnlimiton ] ) && ( [ $CurrentTime -le $vpnlimitoff ] ) ) ) || ( ( [ $vpnlimiton -ge $vpnlimitoff ] ) && ( ( [ $CurrentTime -ge $vpnlimiton ] ) || ( [ $CurrentTime -le $vpnlimitoff ] ) ) ) ) ; then
-        if [ $(uci get simple-tc.mesh_vpn.enabled) -eq 0 ] ; then
+    if ( [ ${#vpnlimiton} -eq 4 ] ) && ( [ ${#vpnlimitoff} -eq 4 ] ); then
+      if ( ( ( [ $vpnlimiton -le $vpnlimitoff ] ) && ( ( [ $CurrentTime -ge $vpnlimiton ] ) && ( [ $CurrentTime -le $vpnlimitoff ] ) ) ) || ( ( [ $vpnlimiton -ge $vpnlimitoff ] ) && ( ( [ $CurrentTime -ge $vpnlimiton ] ) || ( [ $CurrentTime -le $vpnlimitoff ] ) ) ) ); then
+        if [ $(uci get simple-tc.mesh_vpn.enabled) -eq 0 ]; then
           uci set simple-tc.mesh_vpn.enabled=1
           logger -s -t "tecff-vpnlimittimeclock" -p 5 "VPN-bandwidthlimit aktiviert"
           /etc/init.d/network restart
           rm $vpnlimitoff &>/dev/null
           echo 1> $vpnlimiton
-         fi
-       else
-        if [ $(uci get simple-tc.mesh_vpn.enabled) -eq 1 ] ; then
+        fi
+      else
+        if [ $(uci get simple-tc.mesh_vpn.enabled) -eq 1 ]; then
           uci set simple-tc.mesh_vpn.enabled=0   
           logger -s -t "tecff-vpnlimittimeclock" -p 5 "VPN-bandwidthlimit deaktiviert"
           /etc/init.d/network restart
           rm $vpnlimiton &>/dev/null
           echo 1> $vpnlimitoff
-         fi
+        fi
       fi
-     else
+    else
       logger -s -t "tecff-vpnlimittimeclock" -p 5 "simple-tc.mesh_vpn.clock_on or simple-tc.mesh_vpn.clock_off not set correctly to hhmm format."
-     fi
-   fi
- fi
+    fi
+  fi
+fi
 
