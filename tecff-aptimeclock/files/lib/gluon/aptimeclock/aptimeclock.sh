b93672412765799e5645369ee239c6bf3be36bb3
Andreas Ziegler <github@andreas-ziegler.de>
2017-02-01 03:31:36 +0100

    aptimeclock: cleanup & indention fix

--- tecff-aptimeclock/files/lib/gluon/aptimeclock/aptimeclock.sh
+++ tecff-aptimeclock/files/lib/gluon/aptimeclock/aptimeclock.sh
@@ -8,7 +8,6 @@ ClientRadio0off="/tmp/ClientRadio0.off"
 ClientRadio0on="/tmp/ClientRadio0.on"
 
 CurrentTime="$(date +%k%M)"
-CurrentDayOfWeek="$(date +%w)"
 
 dummy=$(uci get wireless.client_radio0.disabled)
 if [ $? -eq 0 ]; then
@@ -16,17 +15,17 @@ if [ $? -eq 0 ]; then
   if [ $? -eq 0 ]; then
     apclock0on=$(uci get wireless.radio0.client_clock_on)
     apclock0off=$(uci get wireless.radio0.client_clock_off)
-    if ( [ ${#apclock0on} -eq 4 ] ) && ( [ ${#apclock0off} -eq 4 ] ) ; then
-      if ( ( ( [ $apclock0on -le $apclock0off ] ) && ( ( [ $CurrentTime -le $apclock0on ] ) || ( [ $CurrentTime -ge $apclock0off ] ) ) ) || ( ( [ $apclock0on -ge $apclock0off ] ) && ( ( [ $CurrentTime -le $apclock0on ] ) && ( [ $CurrentTime -ge $apclock0off ] ) ) ) ) ; then
-        if [ $(uci get wireless.client_radio0.disabled) -eq 0 ] ; then
+    if ( [ ${#apclock0on} -eq 4 ] ) && ( [ ${#apclock0off} -eq 4 ] ); then
+      if ( ( ( [ $apclock0on -le $apclock0off ] ) && ( ( [ $CurrentTime -le $apclock0on ] ) || ( [ $CurrentTime -ge $apclock0off ] ) ) ) || ( ( [ $apclock0on -ge $apclock0off ] ) && ( ( [ $CurrentTime -le $apclock0on ] ) && ( [ $CurrentTime -ge $apclock0off ] ) ) ) ); then
+        if [ $(uci get wireless.client_radio0.disabled) -eq 0 ]; then
           uci set wireless.client_radio0.disabled=1
           logger -s -t "tecff-aptimeclock" -p 5 "APradio0 deaktiviert"
           /sbin/wifi
           rm $ClientRadio0on &>/dev/null
           echo 1> $ClientRadio0off
-         fi
-       else
-        if [ $(uci get wireless.client_radio0.disabled) -eq 1 ] ; then
+        fi
+      else
+        if [ $(uci get wireless.client_radio0.disabled) -eq 1 ]; then
           uci set wireless.client_radio0.disabled=0
           logger -s -t "tecff-aptimeclock" -p 5 "APradio0 aktiviert"
           /sbin/wifi
@@ -34,11 +33,11 @@ if [ $? -eq 0 ]; then
           echo 1> $ClientRadio0on
         fi
       fi
-     else
+    else
       logger -s -t "tecff-aptimeclock" -p 5 "wireless.radio0.client_apclock0on or apclock0off not set correctly to hhmm format."
-     fi
-   fi
- fi
+    fi
+  fi
+fi
 
 dummy=$(uci get wireless.client_radio1.disabled)
 if [ $? -eq 0 ]; then
@@ -46,17 +45,17 @@ if [ $? -eq 0 ]; then
   if [ $? -eq 0 ]; then
     apclock1on=$(uci get wireless.radio1.client_clock_on)
     apclock1off=$(uci get wireless.radio1.client_clock_off)
-    if ( [ ${#apclock1on} -eq 4 ] ) && ( [ ${#apclock1off} -eq 4 ] ) ; then
-      if ( ( ( [ $apclock1on -le $apclock1off ] ) && ( ( [ $CurrentTime -le $apclock1on ] ) || ( [ $CurrentTime -ge $apclock1off ] ) ) ) || ( ( [ $apclock1on -ge $apclock1off ] ) && ( ( [ $CurrentTime -le $apclock1on ] ) && ( [ $CurrentTime -ge $apclock1off ] ) ) ) ) ; then
-        if [ $(uci get wireless.client_radio1.disabled) -eq 0 ] ; then
+    if ( [ ${#apclock1on} -eq 4 ] ) && ( [ ${#apclock1off} -eq 4 ] ); then
+      if ( ( ( [ $apclock1on -le $apclock1off ] ) && ( ( [ $CurrentTime -le $apclock1on ] ) || ( [ $CurrentTime -ge $apclock1off ] ) ) ) || ( ( [ $apclock1on -ge $apclock1off ] ) && ( ( [ $CurrentTime -le $apclock1on ] ) && ( [ $CurrentTime -ge $apclock1off ] ) ) ) ); then
+        if [ $(uci get wireless.client_radio1.disabled) -eq 0 ]; then
           uci set wireless.client_radio1.disabled=1
           logger -s -t "tecff-aptimeclock" -p 5 "APradio1 deaktiviert"
           /sbin/wifi
           rm $ClientRadio0on &>/dev/null
           echo 1> $ClientRadio1off
-         fi
-       else
-        if [ $(uci get wireless.client_radio1.disabled) -eq 1 ] ; then
+        fi
+      else
+        if [ $(uci get wireless.client_radio1.disabled) -eq 1 ]; then
           uci set wireless.client_radio1.disabled=0
           logger -s -t "tecff-aptimeclock" -p 5 "APradio1 aktiviert"
           /sbin/wifi
@@ -64,9 +63,9 @@ if [ $? -eq 0 ]; then
           echo 1> $ClientRadio1on
         fi
       fi
-     else
+    else
       logger -s -t "tecff-aptimeclock" -p 5 "wireless.radio1.client_apclock1on or .apclock1off not set correctly to hhmm format."
-     fi
-   fi
- fi
+    fi
+  fi
+fi
 
