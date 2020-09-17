19c1d4477478959a35f50c4776c15b5d4f26fe5a
Andreas Ziegler <dev@andreas-ziegler.de>
2020-09-17 23:18:50 +0200

    aptimeclock: change create/delete of ON/OFF tempfiles

--- tecff-aptimeclock/files/lib/gluon/aptimeclock/aptimeclock.sh
+++ tecff-aptimeclock/files/lib/gluon/aptimeclock/aptimeclock.sh
@@ -51,16 +51,16 @@ CurrentTime="$(date +%k%M)"
           uci set wireless.client_radio0.disabled=1
           logger -s -t "$SCRIPTNAME" -p 5 "APradio0 deaktiviert"
           /sbin/wifi
-          rm $PUBLIC_WLAN_ON_FILE &>/dev/null
-          echo 1> $PUBLIC_WLAN_OFF_FILE
+          rm -f $PUBLIC_WLAN_ON_FILE &>/dev/null
+          touch $PUBLIC_WLAN_OFF_FILE
         fi
       else
         if [ $(uci get wireless.client_radio0.disabled) -eq 1 ]; then
           uci set wireless.client_radio0.disabled=0
           logger -s -t "$SCRIPTNAME" -p 5 "APradio0 aktiviert"
           /sbin/wifi
-          rm $PUBLIC_WLAN_OFF_FILE &>/dev/null
-          echo 1> $PUBLIC_WLAN_ON_FILE
+          rm -f $PUBLIC_WLAN_OFF_FILE &>/dev/null
+          touch $PUBLIC_WLAN_ON_FILE
         fi
       fi
     else
