5f19531a6276d9f9495edb27ececbc03c6911e82
Andreas Ziegler <dev@andreas-ziegler.de>
2020-11-16 00:48:10 +0100

    aptimeclock: move config sanity check out of the loop

--- tecff-aptimeclock/files/lib/gluon/aptimeclock/aptimeclock.sh
+++ tecff-aptimeclock/files/lib/gluon/aptimeclock/aptimeclock.sh
@@ -41,28 +41,33 @@ fi
 APCLOCK_ON="$(uci get $APCLOCK_CONF_ON)"
 APCLOCK_OFF="$(uci get $APCLOCK_CONF_OFF)"
 
+if ( [ ${#APCLOCK_ON} -ne 4 ] ) || ( [ ${#APCLOCK_OFF} -ne 4 ] ); then
+	logger -s -t "$SCRIPTNAME" -p 5 "client_clock_on and/or client_clock_off not set correctly to hhmm format. aborting."
+	exit 1
+fi
+if [ "$APCLOCK_ON" = "$APCLOCK_OFF" ]; then
+	logger -s -t "$SCRIPTNAME" -p 5 "client_clock_on and client_clock_off are set to the same value. aborting."
+	exit 1
+fi
+
 CurrentTime="$(date +%H%M)"
 
 WLAN_INTERFACES_OPEN="$(uci show wireless | cut -d"." -f2 | egrep "(client|owe)_radio[0-9]$" | uniq | tr '\n' ' ')"
 
 for wlanif in $WLAN_INTERFACES_OPEN; do
-	if ( [ ${#APCLOCK_ON} -eq 4 ] ) && ( [ ${#APCLOCK_OFF} -eq 4 ] ); then
-		# following if clause is separated whether midnight is between the ON/OFF times
-		if ( ( ( [ $APCLOCK_ON -le $APCLOCK_OFF ] ) && ( ( [ $CurrentTime -lt $APCLOCK_ON ] ) || ( [ $CurrentTime -ge $APCLOCK_OFF ] ) ) ) || \
-			( ( [ $APCLOCK_ON -ge $APCLOCK_OFF ] ) && ( ( [ $CurrentTime -lt $APCLOCK_ON ] ) && ( [ $CurrentTime -ge $APCLOCK_OFF ] ) ) ) ); then
-			if [ $(uci get wireless.${wlanif}.disabled) -eq 0 ]; then
-				uci set wireless.${wlanif}.disabled=1
-				logger -s -t "$SCRIPTNAME" -p 5 "${wlanif} deactivated"
-				WLAN_DISABLE_TRIGGER=true
-			fi
-		else # node's time is currently in active public wlan timeframe
-			if [ -f "$PUBLIC_WLAN_OFF_FILE" ]; then # public wlan has been deactivated before by this script
-				logger -s -t "$SCRIPTNAME" -p 5 "${wlanif} activated"
-				WLAN_ENABLE_TRIGGER=true
-			fi
+	# following if clause is separated whether midnight is between the ON/OFF times
+	if ( ( ( [ $APCLOCK_ON -le $APCLOCK_OFF ] ) && ( ( [ $CurrentTime -lt $APCLOCK_ON ] ) || ( [ $CurrentTime -ge $APCLOCK_OFF ] ) ) ) || \
+		( ( [ $APCLOCK_ON -ge $APCLOCK_OFF ] ) && ( ( [ $CurrentTime -lt $APCLOCK_ON ] ) && ( [ $CurrentTime -ge $APCLOCK_OFF ] ) ) ) ); then
+		if [ $(uci get wireless.${wlanif}.disabled) -eq 0 ]; then
+			uci set wireless.${wlanif}.disabled=1
+			logger -s -t "$SCRIPTNAME" -p 5 "${wlanif} deactivated"
+			WLAN_DISABLE_TRIGGER=true
 		fi
-	else
-		logger -s -t "$SCRIPTNAME" -p 5 "client_clock_on or client_clock_off not set correctly to hhmm format."
+	else # node's time is currently in active public wlan timeframe
+		if [ -f "$PUBLIC_WLAN_OFF_FILE" ]; then # public wlan has been deactivated before by this script
+			logger -s -t "$SCRIPTNAME" -p 5 "${wlanif} activated"
+			WLAN_ENABLE_TRIGGER=true
+		fi
 	fi
 done
 if [ "$WLAN_DISABLE_TRIGGER" = true ]; then
